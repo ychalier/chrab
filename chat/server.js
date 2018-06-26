3572e91a7746266a65e2ecb7be50b54ee9bf203f
ychalier <yohan.chalier@telecom-paristech.fr>
2018-06-26 21:30:51 +0200

    basic routing components

--- chat/server.js
+++ chat/server.js
@@ -1,31 +1,76 @@
-var url = require('url');
-var http = require('http');
+const http = require('http');
 
 var port = 8000;
 
-// a map of all views offered by the server
-var routes = {};
+var routes = {
+  'GET': {
 
-routes["/"] = function (query, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello, World!');
-  res.end();
-}
-
-function router(req, res) {
-
-  // logging incoming requests
-  console.log(req.headers.host + "\t" + req.method + "\t" + req.url);
-
-  var target = req.url.split("?")[0];  // the proper URL, a.k.a. the route
-  var query = url.parse(req.url, true).query;  // GET parameters
-
-  if (req.method === "GET") {
-    if (target in routes) {
-      routes[target](query, res);
+    '^/$': function (req, res) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write('Hello, World!');
+      res.end();
     }
+
+  },
+  'POST': {
+
+    '^/$': function (req, res) {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.write(req.body);
+      res.end();
+    }
+
   }
 }
 
+function exitWithErrorCode(res, statusCode) {
+  res.statusCode = statusCode;
+  res.end();
+}
+
+function handleRequest(req, res) {
+
+  const { headers, method, url } = req;
+  console.log(headers.host + "\t" + method + "\t" + url);
+
+  let body = [];
+  req.on('error', (err) => {
+    console.error(err);
+    exitWithErrorCode(res, 400);  // Bad Request
+  }).on('data', (chunk) => {
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    req.body = body;
+
+    res.on('error', (err) => {
+      console.error(err);
+      exitWithErrorCode(res, 500);  // Internal Server Error
+    });
+
+    var routeFound = false;
+    var radix = req.url.split("?")[0];
+    if (method in routes) {
+      for (route in routes[method]) {
+        let regex = new RegExp(route[1], 'i');
+        if (regex.exec(radix)) {
+          routeFound = true;
+          routes[method][route](req, res);
+          break;
+        }
+      }
+    }
+
+    if (!routeFound) {
+      exitWithErrorCode(res, 404);  // Not Found
+    }
+
+  });
+}
+
 console.log("Starting server on port " + port + "...");
-http.createServer(router).listen(port);
+http.createServer(handleRequest).listen(port);
