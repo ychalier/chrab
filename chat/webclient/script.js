776bfe7bd92463f1a5a4b1aeec9825f9d8fdbdc1
ychalier <yohan.chalier@telecom-paristech.fr>
2018-06-29 16:56:28 +0200

    webclient retrieve token

--- chat/webclient/script.js
+++ chat/webclient/script.js
@@ -1,4 +1,4 @@
-var xhttp = new XMLHttpRequest();
+/*var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
     document.getElementById("chat").innerHTML = xhttp.responseText;
@@ -6,3 +6,32 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "http://localhost:8000/", true);
 xhttp.send();
+*/
+
+var token = null;
+
+document.getElementById('form-login-submit')
+        .addEventListener('click', function(event) {
+  event.preventDefault();
+  let xhttp = new XMLHttpRequest();
+  let username =
+    document.querySelector('#form-login > input[type="text"]')
+            .value;
+  let password =
+    document.querySelector('#form-login > input[type="password"]')
+            .value;
+  let hash = btoa(username + ':' + password);
+  xhttp.open("GET", "http://localhost:8000/retrieve-token", true);
+  xhttp.setRequestHeader('Authorization', 'Basic ' + hash);
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      if (xhttp.status == 200) {
+        token = JSON.parse(xhttp.responseText);
+        alert('Connected!');
+      } else {
+        alert(xhttp.status);
+      }
+    }
+  }
+  xhttp.send();
+});
