193de527feca753f3963a43463d93d699b76fe4c
ychalier <yohan.chalier@telecom-paristech.fr>
2018-07-05 18:36:09 +0200

    added old messages removal

--- chat/background.js
+++ chat/background.js
@@ -2,9 +2,23 @@ const sqlite3 = require('sqlite3').verbose();
 
 
 function clearOldMessages() {
-  //TODO: clear old messages
+  let db = new sqlite3.Database('chat.db', (err) => {
+    if (err) { console.error(err); }
+  });
+  db.all('SELECT * FROM channels', [],
+    (err, channels) => {
+      if (err) { console.error(err); }
+      else {
+        let now = new Date().getTime();
+        for (var i = 0; i < channels.length; i++) {
+          let deleteBefore = now - channels[i].delay * 1000;
+          db.run('DELETE FROM messages WHERE t < (?) AND channel = (?)',
+            [deleteBefore, channels[i].id]);
+        }
+      }
+  });
+  db.close();
 }
-
 
 var refresh_rate = 1 * 60 * 1000;  // 1 minute
 var scheduler_state = 0;
--- chat/server.js
+++ chat/server.js
@@ -118,7 +118,7 @@ function load_database() {
     + '(id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, hash TEST, '
     + 'expires INTEGER, username TEXT, t INTEGER)');
   db.run('CREATE TABLE IF NOT EXISTS channels '
-    + '(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)');
+    + '(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, delay INT)');
   db.run('CREATE TABLE IF NOT EXISTS messages '
     + '(id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT, channel INTEGER, '
     + 'username TEXT, t INTEGER)');
