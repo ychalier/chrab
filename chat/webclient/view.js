6888e8f77dc231b03f4c0d11ad819aedb9fe2b78
ychalier <yohan.chalier@telecom-paristech.fr>
2018-08-06 12:27:34 +0200

    webclient base layout & hide/show utilities

---
+++ chat/webclient/view.js
@@ -0,0 +1,26 @@
+var accountPanelState = false;  // true when logged in
+function setAccountPanelState(state) {
+  accountPanelState = state;
+  displayElementsFromState(state,
+    document.getElementById("account-panel-top"));
+  displayElementsFromState(state,
+    document.getElementById("account-panel-bottom"));
+}
+
+function displayElementsFromState(state, parent) {
+  /* parent is an (DOM) element */
+  let childs = parent.childNodes;
+  for (let i = 0; i < childs.length; i++) {
+    let cn = childs[i].className;
+    if (cn && cn.includes("state-" + state)) {  // show it
+      if (childs[i].hasAttribute("display")) {
+        childs[i].style.display = childs[i].getAttribute("display");
+      } else {
+        childs[i].style.display = "block";
+      }
+    } else if (cn && cn.includes("state-" + (!state))) {  // hide it
+      childs[i].setAttribute("display", childs[i].style.display);
+      childs[i].style.display = "none";
+    }
+  }
+}
