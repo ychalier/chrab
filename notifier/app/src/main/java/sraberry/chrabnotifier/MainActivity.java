398a0a16ebab801e063e9839820a47e85d655f87
ychalier <yohan.chalier@telecom-paristech.fr>
2018-08-16 12:44:34 +0200

    linked stop service to main activity & toast messages

--- notifier/app/src/main/java/sraberry/chrabnotifier/MainActivity.java
+++ notifier/app/src/main/java/sraberry/chrabnotifier/MainActivity.java
@@ -3,12 +3,14 @@ package sraberry.chrabnotifier;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 
@@ -18,11 +20,12 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Response.Listener<String> {
 
     public static final String EXTRA_CHANNELS = "channels";
 
     private ArrayList<ChannelFragment> channelFragments;
+    private JSONObject token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,70 +33,93 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
-        final Context context = this;
+
         try {
-            final JSONObject token =
-                    new JSONObject(intent.getStringExtra(LoginActivity.EXTRA_TOKEN));
+            token = new JSONObject(intent.getStringExtra(LoginActivity.EXTRA_TOKEN));
             RequestSender.sendRequest(this, RequestSender.HOST + "/channels",
-                    RequestSender.bearerAuthAccess(token),
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONArray channels = new JSONArray(response);
-                                Log.d("Channels", channels.toString());
-
-                                channelFragments = new ArrayList<>();
-                                LinearLayout channelListLayout =
-                                        findViewById(R.id.layout_channel_list);
-
-                                for (int i = 0; i < channels.length(); i++) {
-                                    JSONObject channelJson = channels.getJSONObject(i);
-                                    ChannelFragment fragment = ChannelFragment.newInstance(
-                                            context,
-                                            channelJson.getString("name"),
-                                            channelJson.getBoolean("protected"),
-                                            token);
-                                    channelFragments.add(fragment);
-                                    FragmentTransaction ft =
-                                            getSupportFragmentManager().beginTransaction();
-                                    ft.add(channelListLayout.getId(), fragment, "fragment");
-                                    ft.commit();
-                                }
-
-                                findViewById(R.id.button_start_service)
-                                        .setOnClickListener(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View v) {
-                                        ArrayList<String> extraChannels = new ArrayList<>();
-                                        for (ChannelFragment channelFragment: channelFragments) {
-                                            if (channelFragment.isChecked()) {
-                                                extraChannels.add(
-                                                        channelFragment.getIntentDetails());
-                                            }
-                                        }
-                                        Intent serviceIntent = new Intent(context,
-                                                NotificationService.class);
-                                        serviceIntent.putExtra(EXTRA_CHANNELS, extraChannels);
-                                        serviceIntent.putExtra(LoginActivity.EXTRA_TOKEN,
-                                                token.toString());
-                                        startService(serviceIntent);
-
-                                        startActivity(new Intent(context,
-                                                BackgroundActivity.class));
-
-                                    }
-                                });
-
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+                    RequestSender.bearerAuthAccess(token), this);
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void onResponse(String response) {
+        final Context context = this;
+        try {
+            JSONArray channels = new JSONArray(response);
+            Log.d("Channels", channels.toString());
+
+            channelFragments = new ArrayList<>();
+            LinearLayout channelListLayout = findViewById(R.id.layout_channel_list);
+
+            for (int i = 0; i < channels.length(); i++) {
+                JSONObject channelJson = channels.getJSONObject(i);
+                ChannelFragment fragment = ChannelFragment.newInstance(
+                        context,
+                        channelJson.getString("name"),
+                        channelJson.getBoolean("protected"),
+                        token);
+                channelFragments.add(fragment);
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.add(channelListLayout.getId(), fragment, "fragment");
+                ft.commit();
+            }
+
+            findViewById(R.id.button_start_service)
+                    .setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (isServiceRunning(NotificationService.class)) {
+                                showToast("Service is already started!");
+                                return;
+                            }
+                            ArrayList<String> extraChannels = new ArrayList<>();
+                            for (ChannelFragment channelFragment: channelFragments) {
+                                if (channelFragment.isChecked()) {
+                                    extraChannels.add(
+                                            channelFragment.getIntentDetails());
+                                }
+                            }
+                            Intent intent = new Intent(context, NotificationService.class);
+                            intent.putExtra(EXTRA_CHANNELS, extraChannels);
+                            intent.putExtra(LoginActivity.EXTRA_TOKEN, token.toString());
+                            startService(intent);
+                            showToast("Service started.");
+                        }
+                    });
+
+            findViewById(R.id.button_stop_service)
+                    .setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            if (!isServiceRunning(NotificationService.class)) {
+                                showToast("Service is not running!");
+                                return;
+                            }
+                            stopService(new Intent(context, NotificationService.class));
+                            showToast("Service stopped.");
+                        }
+                    });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showToast(CharSequence text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service :
+                manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
